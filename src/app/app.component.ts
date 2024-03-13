4a89123cbcf0c9db180e49f3c4a45dc071325842
Augusto Mentz <augustohmn.tech@gmail.com>
2024-03-13 18:48:58 -0300

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Type, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tcc-front-produtos';
+
 }
